12d2faad39393ed1419c77cf5d1a75f54bb48e9c
changjie <doupi.cj@raycloud.com>
2019-03-15 17:19:26 +0800

    #tada#

--- service-provider/src/main/java/me/changjie/service/provider/ServiceProviderApplication.java
+++ service-provider/src/main/java/me/changjie/service/provider/ServiceProviderApplication.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+/**
+ * EnableEurekaClient和EnableDiscoveryClient作用是使应用具有服务注册发现功能
+ * 从Spring Cloud Edgware开始，@EnableDiscoveryClient 或@EnableEurekaClient 可省略。只需加上相关依赖，并进行相应配置，即可将微服务注册到服务发现组件上。
+ * 不同点：@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心
+ */
+//@EnableEurekaClient
 @RestController
 public class ServiceProviderApplication {
 
--- service-ribbon/src/main/java/me/changjie/service/ribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/me/changjie/service/ribbon/ServiceRibbonApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableHystrix
 public class ServiceRibbonApplication {
 
